8bb60b6d8b0a51bf318f201c5fbe45efc65d9a3d
Hassan47HS <2019cs617@student.uet.edu.pk>
2023-06-01 20:39:47 +0500

    Philter

---
+++ Philter App/utils/index.js
@@ -0,0 +1,34 @@
+export const  products={
+    123456789:{
+    Company_name:'Company Name : IT SPAN',
+    Product_Title:"Product Title : It man",
+    Batch_no:"Batch No : 1",
+    MFG_Date:"MFG Date : 20-02-23",
+    EXP_Date:"EXP Date : 20-02-24",
+    Product_Description:"Product Description : Fast pace"
+},
+987654321:{
+    Company_name:'Company Name : cosmetics',
+    Product_Title:"Product Title : 	Good smell",
+    Batch_no:"Batch No : 1",
+    MFG_Date:"MFG Date : 20-02-23",
+    EXP_Date:"EXP Date : 20-02-24",
+    Product_Description:"Product Description : Fast pace"
+},
+543216789:{
+    Company_name:'Company Name : IT worker',
+    Product_Title:"Product Title : 	IT Spans",
+    Batch_no:"Batch No : 12344321",
+    MFG_Date:"MFG Date : 20-02-23",
+    EXP_Date:"EXP Date : 20-02-24",
+    Product_Description:"Product Description : Fast pace"
+},
+// 543219876:{
+//     Company_name:'Company Name : IT SPAN',
+//     Product_Title:"Product Title : It man",
+//     Batch_no:"Batch No : 12344321",
+//     MFG_Date:"MFG Date : 20-02-23",
+//     EXP_Date:"EXP Date : 20-02-24",
+//     Product_Description:"Product Description : Fast pace"
+// }
+}
